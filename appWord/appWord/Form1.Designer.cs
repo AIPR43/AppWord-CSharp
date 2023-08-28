244e0f40fa3c033cc5a4d97c64587c03fd5fe487
AIPR43 <124021410+AIPR43@users.noreply.github.com>
2023-08-28 01:31:24 -0600

    Add files via upload

---
+++ appWord/appWord/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace appWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(23, 31);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(411, 159);
+            this.txtDato.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 336);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ appWord/appWord/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace appWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            dialogo.Filter = "Documentos de Word (*.docx)|*.docx"; // Filtro para seleccionar solo archivos de Word
+            dialogo.DefaultExt = ".docx"; // Extensión predeterminada
+            dialogo.AddExtension = true; // Agregar automáticamente la extensión si no se proporciona
+
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                string ruta = dialogo.FileName; // Obtener la ruta seleccionada por el usuario
+                string dato = txtDato.Text;
+
+                try
+                {
+                    var wordApp = new Word.Application();
+                    wordApp.Visible = true;
+                    var doc = wordApp.Documents.Add();
+                    doc.Content.Text = dato;
+                    doc.SaveAs2(ruta);
+                    doc.Close();
+
+                    MessageBox.Show("Documento guardado exitosamente.", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el documento:\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
